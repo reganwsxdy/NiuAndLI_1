092aeec58452c810d1ce087e9ffd91114df91ad0
reganwsxdy <632272206@qq.com>
2021-04-18 17:09:24 +0800

    默认变更列表

---
+++ src/ExaminationOnly/Wangyi.java
@@ -0,0 +1,18 @@
+package ExaminationOnly;
+
+import java.util.Scanner;
+
+public class Wangyi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++){
+            int w = sc.nextInt();
+            int h = sc.nextInt();
+            char[][] maze = new char[w][h];
+            for (int j = 0; j < h; j++){
+                maze[j] = sc.nextLine().toCharArray();
+            }
+        }
+    }
+}
